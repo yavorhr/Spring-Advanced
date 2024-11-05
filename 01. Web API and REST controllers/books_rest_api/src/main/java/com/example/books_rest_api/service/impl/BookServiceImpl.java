13e08c53256d56addc8eed2d1572f31ab0911c1d
wolfie <yavorhristozov@gmail.com>
2024-11-05 16:56:28 +0200

    Add : BookServiceImpl class as a service implementation
    - Created BookServiceImpl class to implement BookService interface
    - Annotated with @Service for Spring's service layer component scanning

---
+++ 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/service/impl/BookServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.books_rest_api.service.impl;
+
+import com.example.books_rest_api.service.BookService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+}
