f1dd36389f9445ea8e31ef41014877923bb9868c
wolfie <yavorhristozov@gmail.com>
2024-11-05 16:31:27 +0200

    Add : Author entity

---
+++ 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/model/entity/Author.java
@@ -0,0 +1,66 @@
+package com.example.books_rest_api.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.List;
+
+@Entity
+@Table(name = "author")
+public class Author extends BaseEntity {
+  private String firstName;
+  private String lastName;
+  private Integer age;
+  private String publisher;
+  private List<Author> books;
+
+  public Author() {
+  }
+
+ @OneToMany(mappedBy = "author")
+  public List<Author> getBooks() {
+    return books;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public Author setFirstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public Author setLastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public Author setAge(Integer age) {
+    this.age = age;
+    return this;
+  }
+
+  public String getPublisher() {
+    return publisher;
+  }
+
+  public Author setPublisher(String publisher) {
+    this.publisher = publisher;
+    return this;
+  }
+
+  public Author setBooks(List<Author> books) {
+    this.books = books;
+    return this;
+  }
+}
