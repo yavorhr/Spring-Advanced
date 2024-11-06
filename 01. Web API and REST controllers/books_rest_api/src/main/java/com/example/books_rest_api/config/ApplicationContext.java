9fc6d537f9844805c12cfbe8bde13eaa50731c11
wolfie <yavorhristozov@gmail.com>
2024-11-06 15:53:41 +0200

    Update ApplicationContext.java
    Add ModelMapper @Bean

--- 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/config/ApplicationContext.java
+++ 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/config/ApplicationContext.java
@@ -1,7 +1,9 @@
 package com.example.books_rest_api.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.ModelMap;
 
 import java.util.Scanner;
 
@@ -11,5 +13,10 @@ public class ApplicationContext {
   @Bean
   public Scanner scanner() {
     return new Scanner(System.in);
+  }
+
+  @Bean
+  public ModelMapper modelMapper(){
+    return new ModelMapper();
   }
 }
