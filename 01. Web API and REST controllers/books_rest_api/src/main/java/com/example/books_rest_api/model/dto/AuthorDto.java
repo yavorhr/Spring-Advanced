5628fe332066eaaaf220076e5632b68253e9f22d
wolfie <yavorhristozov@gmail.com>
2024-11-06 15:44:11 +0200

    Update AuthorDto.java
    Refact AuthorDto properties

--- 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/model/dto/AuthorDto.java
+++ 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/model/dto/AuthorDto.java
@@ -1,15 +1,16 @@
 package com.example.books_rest_api.model.dto;
 
-import java.util.List;
-
 public class AuthorDto {
+  private Long id;
   private String firstName;
   private String lastName;
-  private Integer age;
-  private String publisher;
-  private List<BookDto> books;
+
 
   public AuthorDto() {
+  }
+
+  public Long getId() {
+    return id;
   }
 
   public String getFirstName() {
@@ -20,40 +21,18 @@ public class AuthorDto {
     return lastName;
   }
 
-  public Integer getAge() {
-    return age;
-  }
-
-  public String getPublisher() {
-    return publisher;
-  }
-
-  public List<BookDto> getBooks() {
-    return books;
-  }
-
   public AuthorDto setFirstName(String firstName) {
     this.firstName = firstName;
     return this;
   }
 
+  public AuthorDto setId(Long id) {
+    this.id = id;
+    return this;
+  }
+
   public AuthorDto setLastName(String lastName) {
     this.lastName = lastName;
-    return this;
-  }
-
-  public AuthorDto setAge(Integer age) {
-    this.age = age;
-    return this;
-  }
-
-  public AuthorDto setPublisher(String publisher) {
-    this.publisher = publisher;
-    return this;
-  }
-
-  public AuthorDto setBooks(List<BookDto> books) {
-    this.books = books;
     return this;
   }
 }
