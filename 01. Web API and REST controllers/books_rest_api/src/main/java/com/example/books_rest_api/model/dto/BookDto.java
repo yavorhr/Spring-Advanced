52239875e7ee89ab0b8e242a37e2378b2bce9fe7
wolfie <yavorhristozov@gmail.com>
2024-11-06 15:43:59 +0200

    Update BookDto.java
    Refact BookDto properties

--- 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/model/dto/BookDto.java
+++ 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/model/dto/BookDto.java
@@ -3,11 +3,26 @@ package com.example.books_rest_api.model.dto;
 import java.time.LocalDate;
 
 public class BookDto {
+  private Long id;
   private String title;
   private LocalDate releaseDate;
   private Integer copies;
+  private AuthorDto author;
+  private String publisher;
 
   public BookDto() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public AuthorDto getAuthor() {
+    return author;
+  }
+
+  public String getPublisher() {
+    return publisher;
   }
 
   public String getTitle() {
@@ -34,6 +49,21 @@ public class BookDto {
 
   public BookDto setCopies(Integer copies) {
     this.copies = copies;
+    return this;
+  }
+
+  public BookDto setAuthor(AuthorDto author) {
+    this.author = author;
+    return this;
+  }
+
+  public BookDto setPublisher(String publisher) {
+    this.publisher = publisher;
+    return this;
+  }
+
+  public BookDto setId(Long id) {
+    this.id = id;
     return this;
   }
 }
