bc3d951c2cbdf5690aea3260ead81c7e8e4a5828
wolfie <yavorhristozov@gmail.com>
2024-11-06 14:47:04 +0200

    Update AuthorRepository.java

--- 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/repository/AuthorRepository.java
+++ 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/repository/AuthorRepository.java
@@ -4,6 +4,10 @@ import com.example.books_rest_api.model.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AuthorRepository  extends JpaRepository<Author,Long> {
+
+  Optional<Author> findByFirstNameAndLastName(String firstName, String lastName);
 }
