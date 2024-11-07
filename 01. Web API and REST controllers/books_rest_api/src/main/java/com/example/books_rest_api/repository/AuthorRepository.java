2fb2bf8e6c110405fdf0a4ff5da23d4f033f613d
wolfie <yavorhristozov@gmail.com>
2024-11-07 13:58:08 +0200

    Update AuthorRepository.java
    change finding author method to Optional<Author> findByFullName(String fullName);

--- 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/repository/AuthorRepository.java
+++ 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/repository/AuthorRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface AuthorRepository  extends JpaRepository<Author,Long> {
 
-  Optional<Author> findByFirstNameAndLastName(String firstName, String lastName);
+  Optional<Author> findByFullName(String fullName);
 }
