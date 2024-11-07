60fbcff39948f94ecb4cba6d1de330066aa11abe
wolfie <yavorhristozov@gmail.com>
2024-11-07 14:04:44 +0200

    Update BookServiceImpl.java

--- 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/service/impl/BookServiceImpl.java
+++ 01. Web API and REST controllers/books_rest_api/src/main/java/com/example/books_rest_api/service/impl/BookServiceImpl.java
@@ -31,33 +31,12 @@ public class BookServiceImpl implements BookService {
   }
 
   @Override
-  public void initBooks() throws IOException {
-    if (this.bookRepository.count() == 0) {
-      Files.readAllLines(Path.of("src/main/resources/static/books"))
-              .forEach(l -> {
-                String[] tokens = l.split(" - ");
-                String bookTitle = tokens[1];
-
-                int year = Integer.parseInt(tokens[2]);
-                LocalDate date = LocalDate.of(year, 1, 1);
-
-                Author author = getAuthorByName(tokens[0]);
-                Integer copies = Integer.parseInt(tokens[3]);
-                String publisher = tokens[4];
-
-                Book book = new Book(bookTitle, date, copies, author, publisher);
-                this.bookRepository.save(book);
-              });
-    }
-  }
-
-  @Override
   public List<BookDto> findAllBooks() {
     return this.bookRepository
             .findAll()
             .stream()
             .map(b -> {
-              BookDto bookDto = this.modelMapper.map(b, BookDto.class);
+              BookDto bookDto = mapToBookDto(b);
               AuthorDto authorDto = this.modelMapper.map(b.getAuthor(), AuthorDto.class);
               bookDto.setAuthor(authorDto);
 
@@ -68,7 +47,9 @@ public class BookServiceImpl implements BookService {
 
   @Override
   public Optional<BookDto> findById(Long id) {
-    return this.bookRepository.findById(id).map(b -> this.modelMapper.map(b, BookDto.class));
+    return this.bookRepository
+            .findById(id)
+            .map(this::mapToBookDto);
   }
 
   @Override
@@ -87,6 +68,36 @@ public class BookServiceImpl implements BookService {
 
     book = this.bookRepository.save(book);
     return book.getId();
+  }
+
+  @Override
+  public void initBooks() throws IOException {
+    if (this.bookRepository.count() == 0) {
+      Files.readAllLines(Path.of("src/main/resources/static/books"))
+              .forEach(l -> {
+                Book book = getBook(l);
+                this.bookRepository.save(book);
+              });
+    }
+  }
+
+  // Helpers
+  private Book getBook(String l) {
+    String[] tokens = l.split(" - ");
+    String bookTitle = tokens[1];
+
+    int year = Integer.parseInt(tokens[2]);
+    LocalDate date = LocalDate.of(year, 1, 1);
+
+    Author author = getAuthorByName(tokens[0]);
+    Integer copies = Integer.parseInt(tokens[3]);
+    String publisher = tokens[4];
+
+    return new Book(bookTitle, date, copies, author, publisher);
+  }
+
+  private BookDto mapToBookDto(Book b) {
+    return this.modelMapper.map(b, BookDto.class);
   }
 
   private Author getAuthorByName(String name) {
