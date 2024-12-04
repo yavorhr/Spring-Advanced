228ed34e9c597cabc1bc99ad729d50679a5c8690
wolfie <yavorhristozov@gmail.com>
2024-12-04 13:17:44 +0200

    Update : errors demo

--- 10. Demos/errors/src/main/java/bg/softuni/errors/web/controllers/OrdersController.java
+++ 10. Demos/errors/src/main/java/bg/softuni/errors/web/controllers/OrdersController.java
@@ -1,15 +1,27 @@
 package bg.softuni.errors.web.controllers;
 
-import bg.softuni.errors.web.exceptions.ObjectNotFoundException;
+
+import bg.softuni.errors.web.exceptions.OrderNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class OrdersController {
 
   @GetMapping("/orders/{id}/details")
-  public String showProductDetails(@PathVariable("id") Long orderId) {
-    throw new ObjectNotFoundException(orderId);
+  public String showProductDetails(@PathVariable("id") String orderId) {
+    throw new OrderNotFoundException("Order with ID " + orderId + " not found.");
+  }
+
+  @ExceptionHandler(OrderNotFoundException.class)
+  public String handleNotFound(OrderNotFoundException ex, Model model, HttpServletResponse response) {
+//    response.setStatus(HttpServletResponse.SC_NOT_FOUND); // Set HTTP status to 404
+    model.addAttribute("notFound", ex.getMessage());
+    return "/error/404";
   }
 }
--- 10. Demos/errors/src/main/java/bg/softuni/errors/web/controllers/ProductController.java
+++ 10. Demos/errors/src/main/java/bg/softuni/errors/web/controllers/ProductController.java
@@ -1,18 +1,34 @@
 package bg.softuni.errors.web.controllers;
 
-
-import bg.softuni.errors.web.exceptions.ObjectNotFoundException;
+import bg.softuni.errors.web.exceptions.OrderNotFoundException;
+import bg.softuni.errors.web.exceptions.ProductNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class ProductController {
 
   @GetMapping("/products/{id}/details")
-  public String showProductDetails(@PathVariable("id") Long productId) {
+  public String showProductDetails(@PathVariable("id") String productId) {
     //retrieve product from repository
     //productRepository.findById(productId).orElseThrow(new ProductNotFoundException());
-    throw new ObjectNotFoundException(productId);
+    throw new ProductNotFoundException("Product with ID " + productId + " not found.");
+  }
+
+  @GetMapping("/products/add")
+  public String  addProduct() {
+    throw new NullPointerException("Product can't be null");
+  }
+
+  @ExceptionHandler(ProductNotFoundException.class)
+  public String handleNotFound(ProductNotFoundException ex, Model model, HttpServletResponse response) {
+    response.setStatus(HttpServletResponse.SC_NOT_FOUND); // Set HTTP status to 404
+    model.addAttribute("notFound", ex.getMessage());
+    return "/error/404";
   }
 }
